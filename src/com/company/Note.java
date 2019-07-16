9d017725b818223d23fbee6a6e534f8f5d8cc87f
Osama <177005@ppu.edu.ps>
2019-07-16 16:45:11 +0300

    Edit Note Date To Be Initialized To current Date

--- src/com/company/Note.java
+++ src/com/company/Note.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class Note
@@ -8,4 +9,16 @@ public class Note
     private String Title;
     private String Text;
     private Date noteDate;
+
+    public Note(int id, String title, String text) {
+        Id = id;
+        Title = title;
+        Text = text;
+        this.noteDate = Calendar.getInstance().getTime(); //getting the current date
+    }
+
+    public Note()
+    {
+        this.noteDate = Calendar.getInstance().getTime(); //getting the current date
+    }
 }
