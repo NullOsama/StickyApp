240246a02d897af6b64f8937faff540f73e5cabb
Waleed Jubeh <wsameer70@gmail.com>
2019-07-16 13:47:28 +0300

    Hello world message

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("HEllo WROrld");
+        System.out.println("FUCK YOU");
     }
 }
