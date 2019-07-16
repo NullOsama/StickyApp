2bfbc5c5f6193cb524b2ee85b795221af0f8ffdd
Osama <177005@ppu.edu.ps>
2019-07-16 16:31:45 +0300

    Setters and Getters for User Class

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -8,9 +8,38 @@ public class User
     private String lName;
     private ArrayList<Note> Notes = null
 
-    public User(String fName, String lName, ArrayList<Note> notes) {
+    public User(String fName, String lName, ArrayList<Note> notes)
+    {
         this.fName = fName;
         this.lName = lName;
+        Notes = notes;
+    }
+    public User()
+    {
+
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public ArrayList<Note> getNotes() {
+        return Notes;
+    }
+
+    public void setNotes(ArrayList<Note> notes) {
         Notes = notes;
     }
 }
